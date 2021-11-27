b1c8bd0a80b559756b98e3888c5990ffb09a7bd5
Shaker1664 <shakerkamal@iut-dhaka.edu>
2021-11-27 21:34:43 +0600

    Initial Commit

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        //GET api/values
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Value>>> Get()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        //GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Value>> Get(int id)
+        {
+            var value = await _context.Values.FindAsync(id);
+            return Ok(value);
+        }
+
+        //POST api/values
+        [HttpPost]
+        public async Task<ActionResult> Post(Value createValue)
+        {
+            var value = new Value()
+            {
+                Id = createValue.Id,
+                Name = createValue.Name
+            };
+
+            _context.Values.Add(value);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(value);
+        }
+
+        //PUT api/value/4
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, Value editValue)
+        {
+            return Ok();
+        }
+
+    }
+}
